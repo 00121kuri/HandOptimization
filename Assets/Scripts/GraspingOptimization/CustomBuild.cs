40fa7f146b26a195df4b288ec72280c4e49a1c58
00121kuri <00121kuri@gmail.com>
2023-12-13 22:55:10 +0900

    Fix: 一時フォルダにビルドしてから指定のフォルダにコピーするように変更

--- Assets/Scripts/GraspingOptimization/CustomBuild.cs
+++ Assets/Scripts/GraspingOptimization/CustomBuild.cs
@@ -10,15 +10,21 @@ namespace GraspingOptimization {
         [MenuItem("Build/Build Client")]
         public static void BuildGameWithVersion()
         {
+            string tempDirectory = "build-app/temp";
+            if (Directory.Exists(tempDirectory))
+            {
+                Directory.Delete(tempDirectory, true);
+            }
+            Directory.CreateDirectory(tempDirectory);
             string updateAppDirectory = LocalConfig.updateAppDirectory;
             // ビルド設定
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
             buildPlayerOptions.scenes = new[] { "Assets/Scenes/HandOpti.unity" }; // ビルドするシーンを指定
-            buildPlayerOptions.locationPathName = $"{updateAppDirectory}/opti-client-v" + Application.version + ".exe"; // アプリ名にバージョンを追加
+            buildPlayerOptions.locationPathName = $"{tempDirectory}/opti-client-v" + Application.version + ".exe"; // アプリ名にバージョンを追加
             buildPlayerOptions.target = BuildTarget.StandaloneWindows64; // ビルドターゲットを指定
             buildPlayerOptions.options |= BuildOptions.Development; // 開発ビルドを有効にする
             buildPlayerOptions.options |= BuildOptions.ConnectWithProfiler; // プロファイラと接続する
-            
+
             // 保存先のディレクトリを削除
             if (Directory.Exists(updateAppDirectory))
             {
@@ -26,10 +32,12 @@ namespace GraspingOptimization {
             }
             // 保存先のディレクトリを作成
             Directory.CreateDirectory(updateAppDirectory);
-            
 
             // ビルドの実行
             BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+            // ビルド後に指定のディレクトリにコピー
+            DirectoryCopier.CopyDirectory(tempDirectory, updateAppDirectory);
 
             // updateInfo.jsonの作成
             UpdateInfo updateInfo = new UpdateInfo();
